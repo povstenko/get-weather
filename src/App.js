7622f6e326fb0ab4dc3f08021b09d9d306a53653
povstenko <vitaly.povstenko@gmail.com>
2019-06-06 18:52:10 +0300

    Add city search
    -create .gitignore

--- src/App.js
+++ src/App.js
@@ -6,18 +6,48 @@ class  App extends React.Component  {
 
   state = {
     temp: undefined,
+    humidity: undefined,
+    temp_max: undefined,
+    temp_min: undefined,
+    pressure: undefined,
     city: undefined
   };
+  
   getWeather = async (e) => {
     e.preventDefault();
-    console.log("Hello");
-    let url = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Kiev&appid=2f340c581eae47ad00eea18ed2125a92&units=metrics')
+    let city = e.target.elements.city.value;
+    console.log(city);
+
+    let url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2f340c581eae47ad00eea18ed2125a92&units=metrics`)
     let data = await url.json();
     console.log(data);
     this.setState(
       {
         temp: data.main.temp,
-        city: data.name
+        temp_max: data.main.temp_max,
+        temp_min: data.main.temp_min,
+        humidity: data.main.humidity,
+        pressure: data.main.pressure,
+        city: data.name,
+
+        press: [
+          {
+            name: 'Freddie',
+            text: 'Hello Freddie'
+          },
+          {
+            name: 'Brian',
+            text: 'Hello Brian'
+          },
+          {
+            name: 'Roger',
+            text: 'Hello Roger'
+          },
+          {
+            name: 'John',
+            text: 'Hello John'
+          }
+        ]
       }
     )
   }
@@ -27,9 +57,15 @@ class  App extends React.Component  {
       <div className="App">
         <InputTag methodWeather = {this.getWeather}/>
         <div className="container">
-          <h3>Forecast</h3>
+        <div className="result">
+        <h3>Forecast</h3>
           <p>City: {this.state.city}</p>
-          <p>Temperature: {this.state.temp}</p>
+          <p>Temperature: {this.state.temp} °C</p>
+          <p>Max temperature: {this.state.temp_max} °C</p>
+          <p>Min temperature: {this.state.temp_min} °C</p>
+          <p>Humidity: {this.state.humidity}</p>
+          <p>Pressure: {this.state.pressure}</p>
+        </div>
         </div>
       </div>
     );
--- src/component/setInput.js
+++ src/component/setInput.js
@@ -7,13 +7,12 @@ class Element extends React.Component {
             <div className="container">
                 <img src="http://www.pngall.com/wp-content/uploads/2017/01/Weather-Report-Free-Download-PNG.png" className="img-fluid" alt="weather"/>
                 <h1>Weather</h1>
-                <form onSubmit={this.props.methodWeather}>
-                    <div className="form-group">
-                        <label >Get Weather forecast anywhere</label>
-                        <input type="text" className="form-control" placeholder="Enter your city"/>
-                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                    </div>
+                <label>Get Weather forecast anywhere</label>
+                <form onSubmit={this.props.methodWeather} className="form-inline md-form mr-auto mb-4">
+                    <div className="">
+                        <input type="text" className="form-control mr-sm-2" placeholder="Enter your city" name="city"/>
                         <button type="submit" className="btn btn-primary">Search</button>
+                    </div>
                 </form>
             </div>
         </div>
